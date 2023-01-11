6f5c88b032ec19bd744af91c3cd27d1d56381043
Boris Urakov <boris.urakov@gmail.com>
2023-01-11 22:13:48 +0300

    Выполнено задание 3.1.1 и 3.1.2

--- task_3/solution.js
+++ task_3/solution.js
@@ -15,7 +15,9 @@ function renderCartItem(item) {
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
     // 1. Создайте переменную itemCountText, равную пустой строке
+    let itemCountText = " ";
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
+    itemCountText = itemCountText + product.count;
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
     // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
